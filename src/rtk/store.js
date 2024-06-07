fc5872018342805e5c3722c30e918201e02d70ae
bidyut-sikder <bidyutsikder420@gmail.com>
2024-06-07 20:47:49 +0600

    editing store

--- src/rtk/store.js
+++ src/rtk/store.js
@@ -7,26 +7,26 @@ import pkg from 'redux-logger';
 import postSlice from "./features/post/postSlice.js";
 const { createLogger } = pkg;
 
-  //console.log(postSlice)
+//console.log(postSlice)
 
 const logger = createLogger()
 
 
 const store = configureStore({
-    reducer: {
-        counter: counterSlice,
-        dynamicCounter: dynamicCounterSlice,
-        //  post: postSlice
+  reducer: {
+    counter: counterSlice,
+    dynamicCounter: dynamicCounterSlice,
+    post: postSlice
 
-    },
-    middleware: (defaultMiddlewares) => defaultMiddlewares().concat(logger)
+  },
+  middleware: (defaultMiddlewares) => defaultMiddlewares().concat(logger)
 
 
 })
 
 
 
- 
+
 
 
 export default store
